c0ad4161225281d6b6fb72649a73f459ab19fd06
Belal hossain <belal1142080@gmail.com>
2020-09-13 08:30:50 +0600

    last

--- vehicleinout/settings.py
+++ vehicleinout/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'vehicleinout.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vehicleinout',
+        'USER': 'postgres',
+        'PASSWORD': 'b4181171100069121',
+        'HOST': 'localhost'
     }
 }
 
